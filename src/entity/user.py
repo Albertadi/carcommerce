5d26fb1ba669afa19f202d73f56c32d39002687d
Albert Adiputra <it@simitclub.github.io>
2024-10-15 20:23:03 +0800

    Modified user account

--- src/entity/user.py
+++ src/entity/user.py
@@ -7,10 +7,11 @@ from .sqlalchemy import db
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, primary_key=True)
     password_hash = db.Column(db.String(128), nullable=False)
     dob = db.Column(db.Date, nullable=False)
+    first_name = db.Column(db.String(100, nullable=False)),
+    last_name = db.Column(db.String(100, nullable=False)),
     user_profile = db.Column(db.String(100), nullable=False)
 
     def set_password(self, password):
@@ -24,8 +25,9 @@ class User(db.Model):
     def to_dict(self):
         """Return a dictionary representation of the user."""
         return {
-            'id': self.id,
-            'name': self.name,
+            'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
             'dob': self.dob.isoformat(),
             'user_profile': self.user_profile
         }
