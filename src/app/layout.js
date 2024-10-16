4784d310e599cba8c2d8c24e066c554849adbd14
kkranezz <da09bros@gmail.com>
2024-10-16 15:35:33 +0800

    Initialized login frontend

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,32 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+const rajdhaniBold = localFont({
+  src: "./fonts/Rajdhani-Bold.ttf",
+  variable: "--font-rajdhani-bold",
+  weight: "700",
+})
+const rajdhaniLight = localFont({
+  src: "./fonts/Rajdhani-Light.ttf",
+  variable: "--font-rajdhani-light",
+  weight: "300",
+})
+const rajdhaniMedium = localFont({
+  src: "./fonts/Rajdhani-Medium.ttf",
+  variable: "--font-rajdhani-medium",
+  weight: "500",
+})
+const rajdhaniRegular = localFont({
+  src: "./fonts/Rajdhani-Regular.ttf",
+  variable: "--font-rajdhani-regular",
+  weight: "400",
+})
+const rajdhaniSemiBold = localFont({
+  src: "./fonts/Rajdhani-SemiBold.ttf",
+  variable: "--font-rajdhani-semi-bold",
+  weight: "600",
+})
+
 export const metadata = {
   title: "CarCommerce",
   description: "hi im description",
@@ -21,7 +47,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${rajdhaniBold.variable} ${rajdhaniLight.variable} ${rajdhaniMedium.variable} ${rajdhaniRegular.variable} ${rajdhaniSemiBold.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function HomePage() {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const userResponse = await axios.get('http://localhost:5000/api/users/1');
+        const userResponse = await axios.get('http://localhost:5000/api/users/2');
         setUser(userResponse.data); 
 
         const messageResponse = await axios.get('http://localhost:5000/api/hello');
@@ -31,24 +31,29 @@ export default function HomePage() {
   }, []);
 
   // Show loading message while data is being fetched
-  if (loading) return <p>Loading user data...</p>;
+  if (loading) return 
+    <p className='flex item-center justify-center bg-[#0E0E17] font-rajdhaniSemiBold text-[#f75049]'>
+      Loading user data...
+    </p>;
 
   // Show error message if there's an error
-  if (error) return <p>{error}</p>;
+  if (error) return <p className='flex item-center justify-center'>{error}</p>;
 
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page.</p>
+    <div className="flex items-center justify-center flex-col text-center min-h-screen items-center h-screen bg-[#0E0E17] font-rajdhaniSemiBold">
+      <h1 className='text-[#f75049] font-rajdhaniBold text-2xl'>WELCOME TO THE HOME PAGE</h1>
+      <hr className='mt-4 border-[#0E0E17]'/>
+      <p className='text-[#f75049] text-lg'>This is the main landing page.</p>
       {/* Link to the login page */}
       <Link href="/pages/login">
-        <p>Go to login</p>
+        <p className='text-[#f75049] text-lg'>Go to LOGINn</p>
       </Link>
       {/* Sample axios fetch from flask */}
-      <h1>{message}</h1>
-      <p><strong>Name:</strong> {user.name}</p>
-      <p><strong>Date of Birth:</strong> {user.dob}</p>
-      <p><strong>User Profile:</strong> {user.user_profile}</p>
+      <h1 className='text-[#f75049] text-lg'>{message}</h1>
+      <p className='text-[#f75049] text-lg'><strong>Name:</strong> {user.name}</p>
+      <p className='text-[#f75049] text-lg'><strong>Date of Birth:</strong> {user.dob}</p>
+      <p className='text-[#f75049] text-lg'><strong>User Profile:</strong> {user.user_profile}</p>
+      <p className='absolute bottom-0 left-0 p-4 text-[#f75049]'>THIS PAGE IS UNDER DEVELOPMENT</p>
     </div>
   );
 }
