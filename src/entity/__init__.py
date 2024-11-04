ede891676de39a713ce02e3c07f0ac62cd613e2e
thebigboss-gif <kennethjosephr7@gmail.com>
2024-11-04 11:52:38 +0800

    commit

--- src/entity/__init__.py
+++ src/entity/__init__.py
@@ -4,7 +4,8 @@ from .profile import Profile
 from .listing import Listing
 from .token import Token
 from .suspension import Suspension
+from .shortlist import Shortlist
 
 __all__ = [
-    "db", "User", "Profile", "Listing", "Token", "Suspension"
+    "db", "User", "Profile", "Listing", "Token", "Suspension", "Shortlist"
 ]
