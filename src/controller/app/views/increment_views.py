42729ef392397938c41bf1fc112d3547df083226
Albertadi <albertadi412@gmail.com>
2024-11-04 12:09:10 +0800

    fixed init

--- src/controller/app/views/increment_views.py
+++ src/controller/app/views/increment_views.py
@@ -1,7 +1,6 @@
 # Libraries
 from flask import Blueprint, request, jsonify
 from src.entity import Views
-from src.entity.listing import Listing  # Import Listing entity to validate listing existence
 
 increment_views_blueprint = Blueprint('increment_views', __name__)
 
@@ -12,10 +11,8 @@ class IncrementViewsController:
         listing_id = data.get('listing_id')
 
         # Increment and retrieve the number of views for the listing
-        views_count = Views.increment_views(listing_id)
+        increment_views_response, status_code = Views.increment_views(listing_id)
 
         return jsonify({
-            'success': True,
-            'listing_id': listing_id,
-            'views_count': views_count
-        }), 200
+            'success': increment_views_response
+        }), status_code
