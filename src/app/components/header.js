0302386c651a187b8f23074a59b0e3fde7abc67c
Albert Adiputra <it@simitclub.github.io>
2024-11-09 16:29:11 +0800

    fixed header

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -31,6 +31,9 @@ export default function Header() {
         router.push('/');
       }
     }
+    else {
+      router.push('/');
+    }
   }
 
   const handleLogin = () => {
--- src/app/pages/buyer/shortlist/page.js
+++ src/app/pages/buyer/shortlist/page.js
@@ -5,7 +5,7 @@ import { Heart, Trash2, Mail, Search } from 'lucide-react';
 import { AuthContext } from '../../authorization/AuthContext';
 import axios from 'axios';
 
-const Shortlist = () => {
+const ShortlistPage = () => {
   const { access_token } = useContext(AuthContext);
   const [searchQuery, setSearchQuery] = useState('');
   const [shortlistedCars, setShortlistedCars] = useState([]);
@@ -197,4 +197,4 @@ const Shortlist = () => {
   );
 };
 
-export default Shortlist;
+export default ShortlistPage;
