f9db08e3c05297df6563736b67af915d3b3ac108
thebigboss-gif <kennethjosephr7@gmail.com>
2024-11-09 14:54:20 +0800

    commit

--- src/controller/app/shortlist/see_num_car_shortlist.py
+++ src/controller/app/shortlist/see_num_car_shortlist.py
@@ -7,15 +7,18 @@ see_num_car_shortlist_blueprint = Blueprint('see_num_car_shortlist', __name__)
 
 class SeeNumCarShortlistController:
     @see_num_car_shortlist_blueprint.route('/api/shortlist/see_num_car_shortlist', methods=['GET'])
-    @permission_required('has_seller_permission')
+    @permission_required('has_sell_permission')
     def see_num_car_shortlist():
         try:
             # Get seller email from JWT token
             seller_data = get_jwt_identity()
             seller_email = seller_data['email']
+            
+            # Get listing_id from query parameters (if provided)
+            listing_id = request.args.get('listing_id')
 
-            # Get total count from Shortlist entity
-            result = Shortlist.count_buyerlistings_onshortlist(seller_email)
+            # Get total count from Shortlist entity, with optional listing_id
+            result = Shortlist.count_buyerlistings_onshortlist(seller_email, listing_id)
             
             return jsonify(result), 200
 
